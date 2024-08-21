0f4e049e51c4891e76c9849c012944b4575a9d41
RIRony9898 <rirony59@gmail.com>
2024-08-21 22:11:02 +0600

    all colors has been set into custom js file

--- js/custom.js
+++ js/custom.js
@@ -3,6 +3,17 @@ tailwind.config = {
       extend: {
         colors: {
           clifford: '#da373d',
+          'green-primary': '#ABEF5F',
+          'green-secondary': 'rgba(171, 239, 95, 0.5)',
+          'dark-primary': '#212121',
+          'dark-secondary': '#494949',
+          'dark-special': '#1C1C1C',
+          'gray': '#737373',
+          'pink': '#E7C1D3',
+          'yellow': '#EFDA6E',
+          'pest': '#A4DAC3',
+          'sky': '#77AAEA',
+          'white': '#FFFFFF',
         }
       }
     }
